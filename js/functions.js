7f3d27803feb9ad4775251ebe5adecb7ca19606c
Adam Mocarski <adammocarski22@gmail.com>
2022-03-02 13:46:27 +0100

    Add script .js

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,17 @@
 function printMessage(msg){
+    
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
+    
+
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
+    
 }
+let playerMover = `papier`;
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + ' Jeśli Twój ruch to papier, to wygrywasz!');
+
---
+++ js/script.js
@@ -0,0 +1,16 @@
+function printMessage(msg){
+    
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+    
+
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+    
+}
+let playerMover = `papier`;
+let computerMove = `kamień`;
+printMessage('Zagrałem ' + computerMove + ' Jeśli Twój ruch to papier, to wygrywasz!');
